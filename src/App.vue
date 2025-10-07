e9858a1a6d79d1a1cfab17ee2114c7ca1151b4d2
Dahlbloms\Arwid <63833@gymnasium.ax>
2025-10-07 06:07:39 +0300

    ladde till missat från sjuk

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,64 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import KnappRad from './components/KnappRad.vue'
+
+const knappar = ref(['Sten', 'Sax', 'Påse'])
+const score = ref({ spelare: 0, dator: 0 })
+const resultat = ref('Du vann!')
+
+function hittaVinnare() {
+  let buttons = document.getElementsByClassName('alternativ')
+  for (let b of buttons) {
+    if (b.classList.contains('spelarval')) {
+      var spelarval = b.textContent
+    }
+    if (b.classList.contains('datorval')) {
+      var datorval = b.textContent
+    }
+  }
+
+  if (spelarval === datorval) {
+    resultat.value = 'Oavgjort'
+  } else if (
+    (spelarval === 'Sten' && datorval === 'Sax') ||
+    (spelarval === 'Sax' && datorval === 'Påse') ||
+    (spelarval === 'Påse' && datorval === 'Sten')
+  ) {
+    resultat.value = 'Du vann'
+    score.value.spelare++
+  } else {
+    resultat.value = 'Du förlorade'
+    score.value.dator++
+  }
+}
+
+function reset() {
+  score.value.dator = 0
+  score.value.spelare = 0
+  let buttons = document.getElementsByClassName('alternativ')
+  for (let b of buttons) {
+    b.title = ''
+    b.classList.remove('spelarval')
+    b.classList.remove('datorval')
+  }
+}
+</script>
 
 <template>
   <header>
-    <h1>sten, sax, påse!</h1>
+    <h1>Sten, sax, påse</h1>
   </header>
 
   <main>
-    <div class="knapprad">
-      <button>Sten</button>
-      <button>Sax</button>
-      <button>Påse</button>
-    </div>
+    <KnappRad :knappar="knappar" />
     <div class="resultat">
-      <p id="resultat">Du vann!</p>
+      <p id="resultat">{{ resultat }}</p>
     </div>
     <div class="score">
-      <p><span id="spelare">0</span> - <span id="dator">0</span></p>
+      <p>
+        <span id="spelare">{{ score.spelare }}</span> - <span id="dator">{{ score.dator }}</span>
+      </p>
+      <button id="nolla" @click="reset">Nollställ poäng</button>
     </div>
   </main>
 </template>
@@ -26,29 +69,19 @@ header {
   margin-bottom: 1.2em;
 }
 
-button {
-  padding: 0.6em 1.2em;
-  font-size: 1.2em;
-  background-color: #f0f0f0;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  cursor: pointer;
-}
-
-.knapprad {
-  display: flex;
-  justify-content: center;
-  gap: 0.6em;
-}
-
 .resultat {
   font-size: 1.2em;
   text-align: center;
-  margin-top: 1.2em 0;
+  margin: 1.2em 0;
 }
-
 .score {
   font-size: 1.2em;
   text-align: center;
+}
+
+#nolla {
+  margin-top: 2em;
+  padding: 0.3em 0.6em;
+  font-size: 0.8em;
 }
 </style>

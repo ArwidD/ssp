fe277870bf582d0f247f1302548e642a927f403a
Dahlbloms\Arwid <63833@gymnasium.ax>
2025-10-07 08:26:11 +0300

    lagt till app funkar å så

--- src/components/KnappRad.vue
+++ src/components/KnappRad.vue
@@ -1,12 +1,17 @@
 <script setup>
+import { ref } from 'vue'
+
 const props = defineProps(['knappar'])
+const emit = defineEmits(['valdaKnappar'])
+
 function spelarval(e) {
   let buttons = document.getElementsByClassName('alternativ')
   for (let b of buttons) {
     b.classList.remove('spelarval')
   }
   e.target.classList.add('spelarval')
-  datorval()
+
+  emit('valdaKnappar', { spelare: e.target.textContent, dator: datorval() })
 }
 
 function datorval() {
@@ -20,6 +25,7 @@ function datorval() {
       b.title = 'Datorns val'
     }
   }
+  return props.knappar[val]
 }
 </script>
 <template>
